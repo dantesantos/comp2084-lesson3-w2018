db8521b045c45f5a461a9702e3cb5e32831938cb
dantesantos <46685889+dantesantos@users.noreply.github.com>
2019-01-23 12:12:14 -0500

    Lesson 3 part 1. Product detaul link and value

--- TheEverythingStore/Controllers/HomeController.cs
+++ TheEverythingStore/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TheEverythingStore.Models;
 
 namespace TheEverythingStore.Controllers
 {
@@ -24,6 +25,39 @@ namespace TheEverythingStore.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult Products()
+        {
+            /*old untyped ViewBag Code
+            var products = new List<string>();
+            //create mock products
+            for(int i = 1; i <= 10; i++)
+            {
+                products.Add("Product " + i.ToString());
+            }
+
+            //pass mock products to the view for display
+            ViewBag.Products = products; */
+
+            var products = new List<Product>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Product product = new Product();
+                product.Name = "Product " + i.ToString();
+                products.Add(product);
+            }
+
+            //load the View and pass the product list to it
+
+            return View(products);
+        }
+
+        public ActionResult ViewProduct(string ProductName)
+        {
+            ViewBag.ProductName = ProductName;
             return View();
         }
     }
---
+++ TheEverythingStore/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TheEverythingStore.Models
+{
+    public class Product
+    {
+        public String Name { get; set; }
+    }
+}
